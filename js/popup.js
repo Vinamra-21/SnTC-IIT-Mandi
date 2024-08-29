d0bb02137e2467010c91cb267a1b9fb4ca0cbd8a
Vinamra Garg <gargvinamra21@gmail.com>
2024-08-30 00:02:24 +0530

    commit

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,18 @@
-const popup = document.getElementById("popup");
+document.addEventListener('DOMContentLoaded', function () {
+    showHome();
+
+    const popup = document.getElementById("popup");
     const closeButton = document.getElementById("close-popup");
 
-    window.onload = function() {
-        popup.style.display = "block";
-    };
+    popup.style.display = "block";
 
     closeButton.onclick = function() {
         popup.style.display = "none";
     };
 
     window.onclick = function(event) {
-        if (event.target == popup) {
+        if (event.target === popup) {
             popup.style.display = "none";
         }
     };
+});
